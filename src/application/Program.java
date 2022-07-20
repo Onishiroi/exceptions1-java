1e0bac9d47d9c487270eda17834eaf7e74710d4a
Onishiroi <moiseseletronica@gmail.com>
2022-07-20 14:17:27 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,7 +11,6 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
-		// teste aaa
 		
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -36,18 +35,13 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates.");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-				
+
+			String error =reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: " + reservation);
+			System.out.println("Reservation: " + reservation);
 			}
 		}
 		
